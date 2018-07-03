cf6ab9b46e3683d7641b21e7b2e23e720aa907ef
KeitaYoshioka <keita.yoshioka@ufz.de>
2018-07-03 14:15:00 +0200

    test for parallel computation

--- ProcessLib/PhaseField/Tests.cmake
+++ ProcessLib/PhaseField/Tests.cmake
@@ -64,3 +64,16 @@ AddTest(
     expected_square_shear_h_400_pcs_0_ts_10000_t_1.000000.vtu square_shear_h_400_pcs_0_ts_10000_t_1.000000.vtu displacement displacement 1e-16 0
     expected_square_shear_h_400_pcs_0_ts_10000_t_1.000000.vtu square_shear_h_400_pcs_0_ts_10000_t_1.000000.vtu phasefield phasefield 1e-16 0
    )
+
+AddTest(
+    NAME LARGE_PhaseField_propagation
+    PATH PhaseField
+    EXECUTABLE ogs
+    EXECUTABLE_ARGS 2D_propagating_petsc_snesVI.prj -- -snes_monitor -ksp_type cg  -pc_type bjacobi -ksp_rtol 1.e-8 -ksp_atol 1.e-10  -snes_atol 1.e-8 -snes_rtol 1.e-8 -snes_max_it 200 -snes_type vinewtonssls
+    WRAPPER time
+    TESTER vtkdiff
+    REQUIREMENTS NOT (OGS_USE_LIS OR OGS_USE_MPI)
+    DIFF_DATA
+    expected_2D_PropagatingCrack_AT1_ell0p03_pcs_1_ts_23_t_0_230000_0.vtu 2D_PropagatingCrack_AT1_ell0p03_pcs_1_ts_23_t_0_230000_0.vtu displacement displacement 1e-16 0
+    expected_2D_PropagatingCrack_AT1_ell0p03_pcs_1_ts_23_t_0_230000_0.vtu 2D_PropagatingCrack_AT1_ell0p03_pcs_1_ts_23_t_0_230000_0.vtu phasefield phasefield 1e-16 0
+   )
