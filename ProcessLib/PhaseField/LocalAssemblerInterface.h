6b5631e099c84966b6e83763d6ca0b0a461906f1
Dmitry Yu. Naumov <github@naumov.de>
2018-10-02 19:03:39 +0200

    Revert "WIP"
    This reverts commit b305810fa989e009f20a14ac0a5af8cf4bc94a8e.

--- ProcessLib/PhaseField/LocalAssemblerInterface.h
+++ ProcessLib/PhaseField/LocalAssemblerInterface.h
@@ -39,11 +39,10 @@ struct PhaseFieldLocalAssemblerInterface
         std::vector<
             std::reference_wrapper<NumLib::LocalToGlobalIndexMap>> const&
             dof_tables,
-        GlobalVector const& x, double const t, bool const use_monolithic_scheme,
+        GlobalVector const& x, double const t, double& crack_volume,
+        bool const use_monolithic_scheme,
         CoupledSolutionsForStaggeredScheme const* const cpl_xs,
-        double& regular_element_crack_volume,
-        std::vector<GlobalIndexType>& ghost_element_ids,
-        std::vector<double>& ghost_element_values) const = 0;
+        GlobalVector& nodal_crack_volume) = 0;
 
     virtual void computeEnergy(
         std::size_t mesh_item_id,
